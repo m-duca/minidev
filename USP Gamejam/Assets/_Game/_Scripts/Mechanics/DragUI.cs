067d84a49422227eed80395f4881db578b010a9e
Matheus Duca <63525232+m-duca@users.noreply.github.com>
2024-09-01 21:56:21 -0300

    Teste

--- USP Gamejam/Assets/_Game/_Scripts/Mechanics/DragUI.cs
+++ USP Gamejam/Assets/_Game/_Scripts/Mechanics/DragUI.cs
@@ -1,13 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class DragUI : MonoBehaviour, IDragHandler
 {
     private static Canvas _windowCanvas;
-
     private RectTransform _rectTransform;
+
+    // Limites da área onde o RectTransform pode se mover (em unidades de tela).
+    public Vector2 minPosition;
+    public Vector2 maxPosition;
 
     private void Awake()
     {
@@ -15,7 +16,21 @@ public class DragUI : MonoBehaviour, IDragHandler
             _windowCanvas = GameObject.FindGameObjectWithTag("Window Canvas").GetComponent<Canvas>();
     }
 
-    private void Start() => _rectTransform = GetComponent<RectTransform>();
-        
-    void IDragHandler.OnDrag(PointerEventData eventData)  => _rectTransform.anchoredPosition += eventData.delta / _windowCanvas.scaleFactor;
+    private void Start()
+    {
+        _rectTransform = GetComponent<RectTransform>();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        // Calcula a nova posição
+        Vector2 newPosition = _rectTransform.anchoredPosition + eventData.delta / _windowCanvas.scaleFactor;
+
+        // Ajusta a posição para ficar dentro dos limites
+        newPosition.x = Mathf.Clamp(newPosition.x, minPosition.x, maxPosition.x);
+        newPosition.y = Mathf.Clamp(newPosition.y, minPosition.y, maxPosition.y);
+
+        // Aplica a posição ajustada
+        _rectTransform.anchoredPosition = newPosition;
+    }
 }
