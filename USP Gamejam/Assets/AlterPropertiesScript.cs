8125207305637ce42d9f158afa1a7e286a0ddf27
Reimyel <caiquegsambrana@gmail.com>
2024-09-02 17:02:39 -0300

    a

--- USP Gamejam/Assets/AlterPropertiesScript.cs
+++ USP Gamejam/Assets/AlterPropertiesScript.cs
@@ -2,14 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class AlterPropertiesScript : MonoBehaviour
 {
     #region Variáveis
     private GameObject _inspectorTab;
 
+    private List<GameObject> _properties = new List<GameObject>();
+
     //Player
     private GameObject _playerProperties;
+    private InputField _pInputFieldX;
+    private InputField _pInputFieldY;
+    private InputField _pInputFieldZ;
 
     //Enemy
     private GameObject _enemyProperties;
@@ -29,41 +35,83 @@ public class AlterPropertiesScript : MonoBehaviour
 
         _playerProperties = GameObject.FindGameObjectWithTag("PlayerProperties");
         _playerProperties.SetActive(false);
+        _properties.Add(_playerProperties);
 
         _enemyProperties = GameObject.FindGameObjectWithTag("EnemyProperties");
         _enemyProperties.SetActive(false);
+        _properties.Add(_enemyProperties);
 
         _obstacleProperties = GameObject.FindGameObjectWithTag("ObstacleProperties");
         _obstacleProperties.SetActive(false);
+        _properties.Add(_obstacleProperties);
 
         _groundProperties = GameObject.FindGameObjectWithTag("GroundProperties");
         _groundProperties.SetActive(false);
+        _properties.Add(_groundProperties);
+
+        SetAllPropertiesActive(false);
+
+        // Referenciar os InputFields para editar a posição do player
+        _pInputFieldX = _playerProperties.transform.Find("InputField_X").GetComponent<InputField>();
+        _pInputFieldY = _playerProperties.transform.Find("InputField_Y").GetComponent<InputField>();
+        _pInputFieldZ = _playerProperties.transform.Find("InputField_Z").GetComponent<InputField>();
+
+        // Adicionar listeners para detectar mudanças nos valores
+        _pInputFieldX.onEndEdit.AddListener(delegate { UpdatePlayerPosition(); });
+        _pInputFieldY.onEndEdit.AddListener(delegate { UpdatePlayerPosition(); });
+        _pInputFieldZ.onEndEdit.AddListener(delegate { UpdatePlayerPosition(); });
     }
     #endregion
 
     #region Funções Próprias
+    private void SetAllPropertiesActive(bool isActive)
+    {
+        foreach (var property in _properties)
+        {
+            property.SetActive(isActive);
+        }
+    }
+
+    private void OpenProperty(GameObject property)
+    {
+        // Desativar todas as propriedades antes de ativar a desejada
+        SetAllPropertiesActive(false);
+
+        _inspectorTab.SetActive(true);
+        property.SetActive(true);
+    }
     public void OpenPlayerProperties()
     {
-        _inspectorTab.SetActive(true);
-        _playerProperties.SetActive(true);
+        OpenProperty(_playerProperties);
     }
 
     public void OpenEnemyProperties()
     {
-        _inspectorTab.SetActive(true);
-        _enemyProperties.SetActive(true);
+        OpenProperty(_enemyProperties);
     }
 
     public void OpenObstacleProperties()
     {
-        _inspectorTab.SetActive(true);
-        _obstacleProperties.SetActive(true);
+        OpenProperty(_obstacleProperties);
     }
 
     public void OpenGroundProperties()
     {
-        _inspectorTab.SetActive(true);
-        _groundProperties.SetActive(true);
+        OpenProperty(_groundProperties);
+    }
+
+    private void UpdatePlayerPosition()
+    {
+        float x = float.Parse(_pInputFieldX.text);
+        float y = float.Parse(_pInputFieldY.text);
+        float z = float.Parse(_pInputFieldZ.text);
+
+        // Atualizar a posição do jogador
+        GameObject player = GameObject.FindWithTag("Player");
+        if (player != null)
+        {
+            player.transform.position = new Vector3(x, y, z);
+        }
     }
     #endregion
 }
